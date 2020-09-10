db46abf084ad5ff16d75e32ce13913798b7a3902
1os4r <50985840+1os4r@users.noreply.github.com>
2020-09-10 11:17:41 +0800

    Update menu.go

--- src/ui/menu.go
+++ src/ui/menu.go
@@ -3,15 +3,15 @@ package ui
 import (
 	"fmt"
 
-	c "github.com/HexChristmas/Christmas/src/color"
-	"github.com/HexChristmas/Christmas/src/version"
+	c "github.com/1os4r/Controls/src/color"
+	"github.com/1os4r/Controls/src/version"
 )
 
 func ShowMenu() {
 	fmt.Println("")
 	fmt.Println(c.YELLOW, "                                     Christmas v"+version.GetVersion())
-	fmt.Println(c.CYAN, "                               	by HexChristmas")
-	fmt.Println(c.CYAN, "                               	Blog: https://HexChristmas.github.io")
+	fmt.Println(c.CYAN, "                               	by 1os4r")
+	fmt.Println(c.CYAN, "                               	Github: https://github.com/1os4r")
 	fmt.Println("")
 	fmt.Println(c.GREEN, " Please use `tab` to autocomplete commands,")
 	fmt.Println(c.GREEN, " or type `exit` to quit this program.")
